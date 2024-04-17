cd49090846091bb26373de4d1e9d70930aaea3bc
Sai Muppa <muppasai21@gmail.com>
2024-04-17 11:51:50 -0500

    Added Terraform configuration for EC2 instance

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example1" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 }
